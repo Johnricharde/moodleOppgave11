87ec4ce0c7da01afd4af23f373e418f477e3452c
John Richard Espedal <117681128+Johnricharde@users.noreply.github.com>
2023-11-15 10:25:38 +0100

    Add project files.

---
+++ moodleOppgave11/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace moodleOppgave11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string myString = "Bob Arno";
+
+            foreach (char character in myString)
+            {
+                Console.WriteLine(character);
+            }
+        }
+    }
+}
